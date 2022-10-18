e315608f4dbbb70348e97d6e38e589f36fd7c3f6
Ahmed Sherwani <ahmed.sherwani@hcl.com>
2022-10-18 11:44:23 -0500

    My second commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        /**
+         * One way of closing a database connection
+         */
         /*try(Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\databases\\testjava.db");
             Statement statement = connection.createStatement() ) {
 
@@ -19,8 +19,33 @@ public class Main
             Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\databases\\testjava.db");
                 Statement statement = connection.createStatement();
                 statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+                //connection.setAutoCommit(false);
 
-                statement.execute("INSERT INTO contacts (name, phone, email)" + "VALUES('Tim', 6545678, 'tim@email.com') ");
+            /**
+             * Lines 31 to 32 are how to insert data into a database
+             */
+                /*
+            statement.execute("INSERT INTO contacts (name, phone, email)" + "VALUES('Jane', 4829484, 'jane@somewhere.com') ");
+            statement.execute("INSERT INTO contacts (name, phone, email)" + "VALUES('Fido', 9038, 'dog@email.com') ");*/
+
+            /**
+             * Line 37 is how to update data in a database
+             */
+           /* statement.execute("UPDATE contacts SET phone = 5566789 WHERE name = 'Jane' ");*/
+
+            /**
+             * Line 42 is how to delete data from a database
+             */
+            /*statement.execute("DELETE FROM contacts WHERE name = 'Joe' ");*/
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+
+            while(results.next())
+            {
+                System.out.println(results.getString("name" ) + " " + results.getInt("phone") + " " + results.getString("email"));
+            }
+            results.close();
 
                 statement.close();
                 connection.close();
